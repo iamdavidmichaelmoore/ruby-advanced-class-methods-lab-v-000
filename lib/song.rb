41eef518d747dbe872abb9aeaaa0e9cd2a336a54
David Moore <hey@iamdavidmoore.com>
2018-05-03 19:56:40 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,14 +32,15 @@ class Song
   def self.find_by_name(name)
     song = ""
     if @@all.include?(name)
-    song = @@all.detect {name}
+    song_index = @@all.index(name)
+    song = @@all[song_index]
     end
     song
   end
 
   def self.find_or_create_by_name(name)
     if @@all.include?(name)
-      @@all.detect {name}
+      @@all.index(name)
     elsif !@@all.include?(name)
       self.create_by_name(name)
     end
