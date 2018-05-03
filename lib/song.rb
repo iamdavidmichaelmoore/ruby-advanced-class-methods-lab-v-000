3445ca5fb9ef354c89530e0701ec5b0896daa117
David Moore <hey@iamdavidmoore.com>
2018-05-03 22:43:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,7 +37,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    found = self.all.name.include?(name) ? self.find_by_name : self.create_by_name
+    found = self.name.all.include?(name) ? self.find_by_name : self.create_by_name
   end
 
 
