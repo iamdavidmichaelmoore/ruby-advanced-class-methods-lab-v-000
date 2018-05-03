287e581b6a24a70798d7cbeecbf229c43760afe4
David Moore <hey@iamdavidmoore.com>
2018-05-03 23:35:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,9 +52,9 @@ class Song
   end
 
   def self.new_from_filename(mp3_filename)
-    artist_title_ary = mp3_filename.split("-").strip
-    artist = artist_title_ary[0]
-    song_title = artist_title_ary[1].split(".")[0]
+    artist_title_ary = mp3_filename.split("-")
+    artist = artist_title_ary[0].strip
+    song_title = artist_title_ary[1]strip.split(".")[0]
     instance = self.find_or_create_by_name(song_title)
     instance.artist_name = artist
     instance
